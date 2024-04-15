15ee2c4af9f79a0b282442ad403fae40ed0c23d4
binsarjr <binsarjr121@gmail.com>
2024-04-15 11:11:23 +0700

    fix(whatsapp-action): remove unwanted dependency
    Removes a dependency that was causing issues.

--- libs/whatsapp-action/src/downloader/youtube-audio-downloader.action.ts
+++ libs/whatsapp-action/src/downloader/youtube-audio-downloader.action.ts
@@ -4,9 +4,7 @@ import { WhatsappMessageAction } from '@app/whatsapp/interfaces/whatsapp.interfa
 import { withSign, withSignRegex } from '@app/whatsapp/supports/flag.support';
 import { getMessageCaption } from '@app/whatsapp/supports/message.support';
 import type { WAMessage, WASocket } from '@whiskeysockets/baileys';
-import { downloadAndConvertToBuffer } from 'src/supports/file.support';
 import { injectRandomHiddenText } from 'src/supports/str.support';
-import { streamToBuffer } from 'src/supports/stream.support';
 import { extractUrls } from 'src/supports/url.support';
 import * as ytdl from 'ytdl-core';
 
@@ -40,26 +38,19 @@ export class YoutubeAudioDownloaderAction extends WhatsappMessageAction {
     await Promise.all(
       urls.map(async (url) => {
         const info = await ytdl.getInfo(url.toString());
-        // const formats = info.formats.filter(
-        //   (format: ytdl.videoFormat) =>
-        //     format.hasAudio &&
-        //     format.container == 'mp4' &&
-        //     format.mimeType.includes('audio'),
-        // );
         const formats = info.formats.filter(
           (format: ytdl.videoFormat) =>
-            format.container === 'mp4' &&
-            new RegExp(/720|480|360|240|270|144/).test(format.qualityLabel) &&
-            format.hasAudio === true,
+            format.hasAudio &&
+            format.container == 'mp4' &&
+            format.mimeType.includes('audio'),
         );
-
-        const stream = await downloadAndConvertToBuffer(formats[0].url);
-        const buffer = await streamToBuffer(stream);
 
         await socket.sendMessage(
           jid,
           {
-            audio: buffer,
+            audio: {
+              url: formats[0].url,
+            },
             mimetype: formats[0].mimeType.split(';')[0],
           },
           { quoted: message },
